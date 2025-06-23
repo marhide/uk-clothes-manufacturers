82c18965bb54e35678f3f29cc39cf1e22453ff32
marhide <louis.smith@hyperoceanic.com>
2025-06-23 20:47:44 +0100

    refactor

--- src/extract.py
+++ src/extract.py
@@ -1,16 +1,20 @@
 import csv
-import pprint
+from pprint import pprint as p
 
-filename_61 = 'data/61 trader search results.csv'
-filename_62 = 'data/62 trader search results.csv'
+def trader_search_result_csv_to_dict():
+    filename_61 = 'data/61 trader search results.csv'
+    filename_62 = 'data/62 trader search results.csv'
 
-filename_list = [filename_61, filename_62]
+    filename_list = [filename_61, filename_62]
 
-data = []
-for filename in filename_list:
-    with open(filename, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for trader in reader:
-            data.append(trader)
+    data = []
+    for filename in filename_list:
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for trader in reader:
+                data.append(trader)
 
-pprint.pprint(data)
+    return data
+
+if __name__ == '__main__':
+    p(trader_search_result_csv_to_dict())
